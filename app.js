f30a1a58ef94105589feb21a97dda9f6684febf1
Aseel Al Dallal <aseelaldallal@gmail.com>
2017-10-04 19:28:24 +0000

    update to ignore empty comments

--- app.js
+++ app.js
@@ -4,19 +4,17 @@ var express           = require("express"),
     randomner         = require('randomner'),
     GooglePlaces      = require('node-googleplaces'),
     fs                = require('fs');
-    
 
 
 
 
 // API KEY
-const places = new GooglePlaces('AIzaSyAvsl7KOmIyQalzMse5idBGElZa8GOdPG0');
+const places = new GooglePlaces(process.env.GOOGLE_API_KEY);
 
 var file = 'campgrounds.json';
 
 var campgrounds = [];
-var currNumCampgrounds = 0
-
+var currNumCampgrounds = 0;
 
 
 /* ------------------------------------------------- */ 
@@ -25,14 +23,14 @@ var currNumCampgrounds = 0
 
 function createCampgroundsJSON() {
     createCampgrounds();
-    for(var i=1; i<20; i++) {
-        setTimeout(function() {createCampgrounds() }, i*3000);
+   for(var i=1; i<42; i++) {
+        setTimeout(function() {createCampgrounds() }, i*2000);
     }
-    setTimeout(function() { 
+    setTimeout(function() {
         var myJSON = JSON.stringify(campgrounds, null, 2);
         console.log("Num: ", currNumCampgrounds);
         record(myJSON);
-    }, 70000);
+    }, 75000)
 };
 
 function createCampgrounds() {
@@ -46,12 +44,11 @@ function createCampgrounds() {
 
 function createLocation(latlng) {
     const params = {
-        //location: latlng,
-        //radius: 50000, //50000 is max
+        location: latlng,
+        radius: 50000, //50000 is max
         types: 'campground'
     };
-    places.details(params, textSearchCallback);
-    // place.textSearch
+    places.textSearch(params, textSearchCallback);
 }
 
 function textSearchCallback(err, response) {
@@ -62,20 +59,43 @@ function textSearchCallback(err, response) {
     var results = response.body.results;
     for (var i = 0; i < results.length; i++) {
         var place = results[i];
-        if(place) { console.log(place);
-            if(!checkIfRecorded(place)) {
+        if(place) { 
+            if(!checkIfRecorded(place) && place.photos) {
                 currNumCampgrounds++;
-                var campground = {};
-                if(place.name) { campground.name = place.name; }
-                if(place.formatted_address) { campground.location = place.formatted_address;}
-                if(place.photos) { campground.image = place.photos[0].photo_reference; }
-                if(place.place_id) { campground.googlePlaceID = place.place_id; }
-                if(place.geometry) {
-                    var placelat = place.geometry.location.lat;
-                    var placelng = place.geometry.location.lng;
-                    campground.latlng = + placelat + ',' + placelng;
-                }
-                campgrounds.push(campground);
+                var query = { placeid: place.place_id}
+                places.details(query, function(err, res) { 
+                    if(err) { console.log(err); }
+                    var thePlace = res.body.result;
+                    var campground = {};
+                    if(thePlace.name) { campground.name = thePlace.name; }
+                    if(thePlace.formatted_address) { campground.location = thePlace.formatted_address;}
+                    if(thePlace.photos) { campground.image = thePlace.photos[0].photo_reference; }
+                    if(thePlace.place_id) { campground.googlePlaceID =thePlace.place_id; }
+                    if(thePlace.geometry) {
+                        var placelat = thePlace.geometry.location.lat;
+                        var placelng = thePlace.geometry.location.lng;
+                        campground.latlng = + placelat + ',' + placelng;
+                    }
+                    var comments = new Array();
+                    if(thePlace.reviews) {
+                        for(var j=0; j<thePlace.reviews.length; j++) {
+                            if(thePlace.reviews[j].text !== "") {
+                                comments.push(thePlace.reviews[j].text);
+                            }
+                        }
+                        campground.comments = comments;
+                    }
+                    if(thePlace.address_components) {
+                        for(var j=0; j<thePlace.address_components.length; j++) {
+                            if(thePlace.address_components[j].types.indexOf('country') !== -1) {
+                                campground.country = thePlace.address_components[j].long_name;
+                            }
+                        }
+                    }
+                    if(campground.country) {
+                        campgrounds.push(campground);
+                    }
+                });
             }
         }
     }
@@ -83,14 +103,11 @@ function textSearchCallback(err, response) {
 
 function checkIfRecorded(place) {
     var id = place.place_id;
-    console.log("ID: ", id);
     for(var i=0; i<campgrounds.length; i++) {
         if(campgrounds[i].googlePlaceID === id) {
-            console.log("RECORDED");
             return true;
         }
     }
-    console.log("returning false");
     return false;
 }
 
@@ -108,5 +125,9 @@ function record(string) {
 /***************************************************/
 
 
-
+/*var query = { placeid: 'ChIJZcWZbicNXBURGX0eqoK-neM' }
+places.details(query, function(err, res) { 
+    console.log(JSON.stringify(res.body, null,2));
+});
+*/
 createCampgroundsJSON(); 
